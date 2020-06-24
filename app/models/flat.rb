b69a45a2ae103f973a62ce2b5b794df045073d06
ludovicfourcroy <ludovicfourcroy8@gmail.com>
2020-06-24 14:04:24 +0200

    search bar without css

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,16 @@
 class Flat < ApplicationRecord
   belongs_to :user
   has_one :flat
+
+  geocoded_by :full_address
+  after_validation :geocode, if: :will_save_change_to_address?
+  include PgSearch::Model
+  pg_search_scope :search_by_address_and_city,
+    against: [:address, :city],
+    using: {
+      tsearch: { prefix: true }
+    }
+  def full_address
+    return "#{self.address}, #{self.city}"
+  end
 end
