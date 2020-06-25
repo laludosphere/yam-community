227ffce305fd6a3dddc124383647765eeb750d67
lorraine bayard <bayardlorraine@gmail.com>
2020-06-25 14:20:27 +0200

    WIP Profile Page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,11 @@
 class ProfilesController < ApplicationController
   def index
-  @users
+  @users = User.all
   end
 
   def show
     @current_user
+    @last_review = Review.where(receiver_id: current_user).last
   end
+
 end
