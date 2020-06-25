d7fadb87836d881d793a8f120eec85df7dd769ce
Louis Adam <louis.adam@ieseg.fr>
2020-06-26 01:14:33 +0200

    authorize policy for charoom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,10 +1,11 @@
 class ChatroomsController < ApplicationController
   def index
-    @chatrooms = Chatroom.where(user: current_user).or(Chatroom.where(flat: current_user.flats))
+    @chatrooms = policy_scope(Chatroom).where(user: current_user).or(Chatroom.where(flat: current_user.flats))
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    authorize @chatroom
   end
 end
