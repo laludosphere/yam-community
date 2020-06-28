2222d4b652a0d75d2277e8bf6600b8ad2429d1d2
yann pfeiffer <pfeifferyann@gmail.com>
2020-06-28 22:36:44 +0200

    Footer updated, chat design much better, and route done

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,8 @@
 class ChatroomsController < ApplicationController
   def index
     @chatrooms = policy_scope(Chatroom).where(user: current_user).or(Chatroom.where(flat: current_user.flats))
+    @last_chatroom = Message.last.chatroom
+    redirect_to chatroom_path(@last_chatroom )
   end
 
   def show
