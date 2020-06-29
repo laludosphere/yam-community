03312bfbf923b8b7e1b0467d20900fd14d8741d8
Louis Adam <louis.adam@ieseg.fr>
2020-06-29 17:17:35 +0200

    profiles routes and show of another user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,10 @@ class ProfilesController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
+    @last_review = @user.received_reviews.last
+    @reviewer = @last_review.reviewer
+    @prefere
   end
 
   def my_profile
@@ -13,5 +16,11 @@ class ProfilesController < ApplicationController
     @last_review = Review.where(receiver_id: current_user).last
     @reviewer = User.find(@last_review.reviewer_id)
     @preference = current_user.preference
+  end
+
+  private
+
+  def review_profile
+    params.require(:profile).permit(:user, :profile_id, :reviews)
   end
 end
