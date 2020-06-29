204c924dacce17517ebb19fea1331e0dd058f21e
yann pfeiffer <pfeifferyann@gmail.com>
2020-06-29 11:52:20 +0200

    chat is functional

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -4,12 +4,23 @@ const initChatroomCable = () => {
   const btnEnvoyer = document.getElementById("envoie");
   if (btnEnvoyer){
     btnEnvoyer.scrollDown = btnEnvoyer.scrollHeight;
-    const messagesContainer = document.getElementById('messages');
+    const messagesElement = document.getElementById('messages')
+    const messagesContainer = document.getElementById('new_message');
     if (messagesContainer) {
-      const id = messagesContainer.dataset.chatroomId;
+      const receiverId = document.getElementById('current_user').dataset.id
+      const id = messagesElement.dataset.chatroomId;
+
       consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
         received(data) {
-          messagesContainer.insertAdjacentHTML('beforeend', data);
+          console.log(data)
+
+          const divElement = document.createElement('div');
+          divElement.insertAdjacentHTML('beforeend', data);
+          const senderId = divElement.querySelector('#sender').dataset.id;
+
+          if (senderId !== receiverId) {
+            messagesContainer.insertAdjacentHTML('beforebegin', data);
+          }
         },
       });
     }
