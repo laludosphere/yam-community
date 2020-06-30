cb4f16e02248f78fc41701c4ed414af0edfd535a
yann pfeiffer <pfeifferyann@gmail.com>
2020-06-30 10:00:41 +0200

    flatsearch

--- app/javascript/plugins/init_flat_modal.js
+++ app/javascript/plugins/init_flat_modal.js
@@ -1,18 +1,18 @@
 const initFlatModal = () => {
-  const search = document.getElementById("flat-form");
-  const flats = document.getElementById("flats-modal");
-  console.log("test")
+  // const search = document.getElementById("flat-form");
+  // const flats = document.getElementById("flats-modal");
+  // console.log("test")
 
-  if (flats){
-    search.addEventListener('submit', event => {
-      event.preventDefault();
-       console.log("test3")
-      flats.classList.remove('d-none');
-      // ('html,body').animate({
-      //     scrollTop: $(".searched-flats").offset().top},
-      //     'slow')
-    });
-  };
+  // if (flats){
+  //   search.addEventListener('submit', event => {
+  //     event.preventDefault();
+  //      console.log("test3")
+  //     flats.classList.remove('d-none');
+  //     // ('html,body').animate({
+  //     //     scrollTop: $(".searched-flats").offset().top},
+  //     //     'slow')
+  //   });
+  // };
 }
 
 export {initFlatModal}
