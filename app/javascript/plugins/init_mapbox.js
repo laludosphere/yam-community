b7f2fcee4156302d8726aea47181265f1dfb191c
yann pfeiffer <pfeifferyann@gmail.com>
2020-07-02 00:00:46 +0200

    markers are well alligned

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,10 +1,21 @@
 import mapboxgl from 'mapbox-gl';
+const mapElement = document.getElementById('map');
 
 const initMapbox = () => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoieWFubjE3MjIiLCJhIjoiY2thczk1aXFsMDV0YTJ4bzM2ZWE5dGwyMiJ9.HI2yIj_FFj0antefS11O3Q';
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
     const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11'
-});}
+
+  });
+
+const markers = JSON.parse(mapElement.dataset.markers);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+    .setLngLat([ marker.lng, marker.lat ])
+    .addTo(map);
+  });
+}
 
 export { initMapbox };
