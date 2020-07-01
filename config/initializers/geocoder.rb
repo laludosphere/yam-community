811840ae79ce6d686fac762c442d831c908cbc43
yann pfeiffer <pfeifferyann@gmail.com>
2020-07-01 15:01:54 +0200

    align

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -17,9 +17,9 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-   units: :km,                 # :km for kilometers or :mi for miles
+  units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
-   lookup:    :google,
-   api_key:   ENV['GOOGLE_API_SERVER_KEY'],
-   use_https: true,
+  lookup:    :google,
+  api_key:   ENV['GOOGLE_API_SERVER_KEY'],
+  use_https: true,
 )
