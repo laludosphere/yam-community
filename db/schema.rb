b950c85dc71609f7b20aa5d328b55f16a04be5ba
Louis Adam <louis.adam@ieseg.fr>
2020-07-02 14:43:07 +0200

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -141,7 +141,7 @@ ActiveRecord::Schema.define(version: 2020_07_02_072809) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone_number"
-    t.string "first_name"
+    t.string "name"
     t.date "born_at"
     t.string "gender"
     t.text "description"
@@ -150,6 +150,7 @@ ActiveRecord::Schema.define(version: 2020_07_02_072809) do
     t.boolean "status_verified", default: false
     t.boolean "owner", default: false
     t.string "last_name"
+    t.string "first_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
